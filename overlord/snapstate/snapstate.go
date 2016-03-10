70c10453efd9cbb9b5f3b88e467e33fc30294b74
Michael Vogt <mvo@ubuntu.com>
2016-03-10 09:54:56 +0100

    update skeleton for snapmgr

---
+++ overlord/snapstate/snapstate.go
@@ -0,0 +1,36 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package snapstate implements the manager and state aspects responsible for the installation and removal of snaps.
+package snapstate
+
+type Status int
+
+const (
+	NeedsDownload Status = 0
+	Downloaded
+	Unpacked
+	Configured
+)
+
+type snapState struct {
+	Name    string
+	Status  Status
+	DevMode bool `json:"dev-mode,omitempty`
+}
